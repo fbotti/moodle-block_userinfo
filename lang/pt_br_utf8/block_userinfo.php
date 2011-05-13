f57a0e66e95faa87009d5e7753fe06ed6a03eb2f
Federico J. Botti <fbotti@gmail.com>
2011-05-13 18:09:15 -0300

    Add pt_br_utf8 translation.

---
+++ lang/pt_br_utf8/block_userinfo.php
@@ -0,0 +1,12 @@
+<?php
+    $string['pluginname'] = 'Bloco de informa&ccedil;&atilde;o de usuario';
+    $string['userinfo'] = 'Informa&ccedil;&atilde;o de usuario';
+    $string['strftimedate'] = 'l jS \de F Y h:i:s A';
+    $string['mycourses'] = 'Meus cursos';
+    $string['editmyprofile'] = 'Modificar perfil';  
+    $string['messages'] = 'Mensagens'; 
+    /* Salute */
+    $string['morning'] = 'Bom dia';
+    $string['afternoon'] = 'Boa tarde';
+    $string['night'] = 'Boa noite';
+?>
