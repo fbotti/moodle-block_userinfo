5251a323d2e69af9eacbbe8c04adea45d5e4e0b1
Federico J. Botti <federico@entornos.com.ar>
2011-05-29 14:21:40 -0300

    Add The Chinese Translation(zh_cn)
    Many thanks to ?? ? (http://moodle.org/user/profile.php?id=1313729)
    for the translation.

---
+++ lang/zh_cn/block_userinfo.php
@@ -0,0 +1,15 @@
+<?php
+    
+    $string['pluginname'] = '用户信息版块'; 
+    $string['userinfo'] = '用户信息';
+    $string['strftimedate'] = "Y年m月d日 h:i:s";
+    $string['mycourses'] = '我的课程';
+    $string['editmyprofile'] = '编辑我的资料';
+    $string['messages'] = '消息';
+
+    /* Salute */
+    $string['morning'] = '早上好'; 
+    $string['afternoon'] = '下午好'; 
+    $string['night'] = '晚上好'; 
+    
+?>
